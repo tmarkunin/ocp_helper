fb3080556f44a5f39157666fce16dbb403658904
Timur Makarov <tmakarov78@yandex.ru>
2020-12-17 19:31:13 +0300

    change static site to node.js

---
+++ python_sample/static_site/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var os = require('os');
+var hostname = os.hostname();
+
+//get port from an environment variable
+const PORT = process.env.PORT || 3000;
+
+
+
+app.get('/dynamic', function(req,res){ 
+         
+    res.send("Static content from " + hostname );
+
+});
+
+
+var server = app.listen(PORT, function(){
+    console.log('Server running on port %d', PORT);
+});
