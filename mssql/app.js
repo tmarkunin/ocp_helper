866b6892b4647e62a073f295bef4725d57d20176
Timur Makarov <tmakarov78@yandex.ru>
2021-01-19 12:49:42 +0300

    mssql test added

---
+++ mssql/app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+
+
+
+//get port from an environment variable
+const PORT = process.env.PORT || 3000;
+
+//mssql
+const sql = require('mssql')
+
+
+
+
+app.get('/healthz', function(req,res){ 
+        	
+    res.send("Working!!!!!.");
+
+});
+
+app.get('/mssql', function(req,res){ 
+
+    async () => {
+        try {
+            // make sure that any items are correctly URL encoded in the connection string
+            await sql.connect('mssql://test_openshift:0pen$hifD@dev-aa-db.ural.evraz.com/kgok-7438-dev')
+            const result = await sql.query`select * from openshift`
+            console.dir(result)
+            res.send(result);
+        } catch (err) {
+            // ... error checks
+        }
+    }
+        	
+
+});
+
+var server = app.listen(PORT, function(){
+    console.log('Server running on port %d', PORT);
+});
+
---
+++ mssql/helpers.sh
@@ -0,0 +1,3 @@
+docker build -t tmarkunin/test_mssql:1.0 .
+docker push tmarkunin/test_mssql:1.0
+
